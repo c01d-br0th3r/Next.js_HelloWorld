2c2a8c774cfc43845eaad3226dec75ad0f7c88d8
c01d-br0th3r <60615552+c01d-br0th3r@users.noreply.github.com>
2020-12-18 14:35:36 +0900

    FIrst Next.js: Dynamic Routing

---
+++ pages/index.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Title = styled.div`
+    font-size: 48px;
+`;
+
+const Home = () => {
+    return <Title>My Home Page</Title>
+}
+
+export default Home;
