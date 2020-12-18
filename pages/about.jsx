2844a09eb97cdb354cb857850f401c042852ee93
c01d-br0th3r <60615552+c01d-br0th3r@users.noreply.github.com>
2020-12-18 19:45:15 +0900

    style: Rewrite lib name (Typo)

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,5 +1,14 @@
+import React, { useState } from "react";
+
 const About = () => {
-    return <div>This is About page</div>
-}
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <div>This is About page.</div>
+      <div>{count}</div>
+      <button onClick={() => setCount((count) => count + 1)}>+</button>
+    </div>
+  );
+};
 
 export default About;
